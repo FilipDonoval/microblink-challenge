9c2ec7d6727e6610486e4e744d8e347b5dc53fbc
FilipDonoval <filip.donoval@gmail.com>
2026-02-04 03:49:47 +0100

    add input for url and submit to fetch the api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,71 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [health, setHealth] = useState<any>(null)
+    const [data, setData] = useState<any>(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [inputUrl, setInputUrl] = useState("")
 
-  const checkHealth = async () => {
-    try {
-      const res = await fetch('/api/health')
-      const data = await res.json()
-      setHealth(data)
-    } catch (err) {
-      console.error(err)
-      setHealth({ error: 'Failed to fetch' })
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setIsLoading(true)
+
+        try {
+            const response = await fetch('/api/scan-repo', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    repo_url: inputUrl
+                }),
+            })
+
+            if (!response.ok) {
+                throw new Error(`Server responded with ${response.status}`)
+            }
+
+            const responseData = await response.json()
+            setData(responseData)
+        } catch (error) {
+            setData((error as Error).message);
+        } finally {
+            setIsLoading(false);
+        }
+
     }
-  }
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={checkHealth}>
-          Check Health
-        </button>
-      </div>
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input style={{width: '50vw'}} type="text" value={inputUrl} disabled={isLoading} onChange={(e) => setInputUrl(e.target.value)}/>
 
-      <div>
-        {health ? (<pre>{JSON.stringify(health, null, 2)}</pre>) : (<p>No health data yet</p>)}
-      </div>
-    </>
-  )
+                <button type="submit" disabled={isLoading || !inputUrl}>
+                    {isLoading ? <div className="spinner"></div> : 'Submit'}
+                </button>
+            </form>
+
+            <div>
+                {data ? (<pre>{JSON.stringify(data, null, 2)}</pre>) : (<p>No data yet</p>)}
+            </div>
+<style>{`
+        .spinner {
+          width: 16px;
+          height: 16px;
+          border: 2px solid #ffffff;
+          border-top: 2px solid transparent;
+          border-radius: 50%;
+          animation: spin 0.8s linear infinite;
+          display: inline-block;
+        }
+
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      `}</style>
+
+        </>
+    )
 }
 
 export default App
